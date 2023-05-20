83a11df08f95335f22af8c2a208e4bfc1ed153ec
José Ignacio Elzo <joselzo@MacBook-Pro.local>
2023-05-20 15:13:36 +0200

    adding new pages

--- course.js
+++ course.js
@@ -1,9 +1,25 @@
 const prompt = require("prompt-sync")();
 
-var name = prompt("Hello, enter you name: ")
-//var messageCount = message.length
-var firstL = name.slice(0,1)
-var fToUpper = firstL.toUpperCase()
-var rname = name.slice(1,15)
-var rtoLower = rname.toLocaleLowerCase ()
-console.log("Hello " + fToUpper + rtoLower)
+// var name = prompt("Hello, enter you name: ")
+// //var messageCount = message.length
+// var firstL = name.slice(0,1)
+// var fToUpper = firstL.toUpperCase()
+// var rname = name.slice(1,15)
+// var rtoLower = rname.toLocaleLowerCase ()
+// console.log("Hello " + fToUpper + rtoLower)
+
+
+
+function ImprimeLista() {
+    let lista = ["perro", "gato", "hamster", "tortuga"];
+    for (let recorrido = 0; recorrido < lista.length; recorrido++) {
+        if (recorrido === 1) {
+            console.log("nulo");
+        } else {
+            let imprimir = lista[recorrido];
+            console.log(imprimir);
+        }
+    }
+}
+
+ImprimeLista();
